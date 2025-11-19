10d56fe3c633b9fc997fa1702eb12f912de8439e
EMMANUEL08161823021 <117604958+EMMANUEL08161823021@users.noreply.github.com>
2025-11-19 11:34:39 +0100

    nextjs-tailwind initiator

---
+++ nextwind/app/page.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      Hello World
+      <p className="border border-white text-center text-red-500 bg-red">This is NexTs</p>
+    </div>
+  );
+}
